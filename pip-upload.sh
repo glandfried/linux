8ad9dccc173b58bf50ded7d781651b877d462ae9
Gustavo Landfried <gustavolandfried@gmail.com>
2021-08-05 12:47:27 -0300

    find, users, pip-upload

---
+++ pip-upload.sh
@@ -0,0 +1,5 @@
+#0. Simply upload your new code to github.
+#0. Create a new release
+#0. Adapt the setup.py file (new download_url — according to your new release tag, new version)
+#0. Run the setup.py: python setup.py sdist
+#0. Rub the twin command again: twine upload dist/*
