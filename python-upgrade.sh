6d1a941cf3409ac4da31d04c209ae66de4ba3e84
Gustavo Landfried <gustavolandfried@gmail.com>
2021-10-24 06:50:24 -0300

    python-upgrade: change the default python3 version

--- python-upgrade.sh
+++ python-upgrade.sh
@@ -4,3 +4,10 @@ sudo -H apt install python3.9-distutils
 sudo -H python3.9 -m pip install --upgrade pip
 sudo -H python3.9 -m pip install --upgrade setuptools
 sudo -H python3.9 -m pip install -r installed.txt
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 2
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9 1
+sudo update-alternatives --config python3 # Enter 2 for Python 3.9
+# Test
+python3 --version
+
+
