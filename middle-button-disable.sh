8a18ab08c278149e8b9780442ee992da2051489e
Gustavo Landfried <gustavolandfried@gmail.com>
2022-08-04 14:43:23 +0200

    Setup 22.04, middle button

---
+++ middle-button-disable.sh
@@ -0,0 +1,2 @@
+# https://askubuntu.com/questions/1405449/disable-middle-click-with-wayland
+gsettings set org.gnome.desktop.peripherals.touchpad middle-click-emulation true
---
+++ setup.sh
@@ -0,0 +1,21 @@
+sudo apt update
+sudo apt upgrade
+sudo apt install git
+ssh-keygen
+sudo apt install tig
+sudo apt install htop
+sudo apt install texlive
+sudo apt install texlive-extra-utils
+sudo apt install texlive-pictures 
+sudo apt install texlive-lang-spanish
+sudo apt install texlive-lang-english
+sudo apt install texlive-lang-french
+wget https://julialang-s3.julialang.org/bin/linux/x64/1.7/julia-1.7.3-linux-x86_64.tar.gz
+tar zxvf julia-1.7.3-linux-x86_64.tar.gz
+mv julia-1.7.3  /opt/julia-1.7.3
+sudo ln -s /opt/julia-1.7.3/bin/julia /usr/local/bin/julia
+sudo apt install r-base-core
+sudo apt install python3-pip
+sudo apt install kate
+sudo apt install inkscape
+sudo apt install retext
