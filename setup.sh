cdfc0bc3d76019c52427c5c49bb9fa69167c5ffa
Gustavo Landfried <gustavolandfried@gmail.com>
2023-08-28 13:54:15 -0300

    Julia

--- setup.sh
+++ setup.sh
@@ -10,10 +10,10 @@ sudo apt install texlive-pictures
 sudo apt install texlive-lang-spanish
 sudo apt install texlive-lang-english
 sudo apt install texlive-lang-french
-wget https://julialang-s3.julialang.org/bin/linux/x64/1.7/julia-1.7.3-linux-x86_64.tar.gz
-tar zxvf julia-1.7.3-linux-x86_64.tar.gz
-mv julia-1.7.3  /opt/julia-1.7.3
-sudo ln -s /opt/julia-1.7.3/bin/julia /usr/local/bin/julia
+wget https://julialang-s3.julialang.org/bin/linux/x64/1.9/julia-1.9.3-linux-x86_64.tar.gz
+tar zxvf julia-1.9.3-linux-x86_64.tar.gz
+mv julia-1.9.3  /opt/julia-1.9.3
+sudo ln -s /opt/julia-1.9.3/bin/julia /usr/local/bin/julia
 sudo apt install r-base-core
 sudo apt install python3-pip
 sudo apt install kate
